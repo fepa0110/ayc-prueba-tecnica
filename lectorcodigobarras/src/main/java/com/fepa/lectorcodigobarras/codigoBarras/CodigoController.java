3f8d8a355fb4def331392e09537bad74138b989e
Fabricio <fepafumi97@gmail.com>
2025-01-12 16:23:43 -0300

    refactor: :recycle: Eliminada redundancia de endpoint y cambiado parametro de ruta
    Se elimino una redundancia en la dirección de la ruta del endpoint y se cambio el tipo de parametro a PathVariable

--- lectorcodigobarras/src/main/java/com/fepa/lectorcodigobarras/codigoBarras/CodigoController.java
+++ lectorcodigobarras/src/main/java/com/fepa/lectorcodigobarras/codigoBarras/CodigoController.java
@@ -2,6 +2,7 @@ package com.fepa.lectorcodigobarras.codigoBarras;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,8 @@ public class CodigoController {
     @Autowired
     private CodigoBarrasService codigoBarrasService;
 
-	@GetMapping("/codigo")
-	public CodigoBarras getCodigoBarras(@RequestParam(value = "codigo", defaultValue = "") String codigoEscaneado) {
+	@GetMapping("/{codigo}")
+	public CodigoBarras getCodigoBarras(@PathVariable(value = "codigo") String codigoEscaneado) {
         return codigoBarrasService.getCodigoBarrasData(codigoEscaneado);
 	}
 }
