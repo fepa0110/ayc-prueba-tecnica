6afa2ee71c95ae80d9188060165da10a91180c96
Fabricio <fepafumi97@gmail.com>
2025-01-12 16:10:55 -0300

    refactor: :recycle: Extraida palabra final de codigo a constante

--- lectorcodigobarras/src/main/java/com/fepa/lectorcodigobarras/codigoBarras/CodigoBarrasService.java
+++ lectorcodigobarras/src/main/java/com/fepa/lectorcodigobarras/codigoBarras/CodigoBarrasService.java
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CodigoBarrasService {
-    public CodigoBarras getCodigoBarrasData(String codigo){
-        if(codigo.length() == 12 || codigo.length() == 13 && codigo.endsWith("ACME")){
-            String lote_extendido = codigo.substring(0, 6);
-            String bulto = "";
+    private final String PALABRA_FINAL = "ACME";
 
-            if(codigo.length() == 12) bulto = codigo.substring(6, 8);
-            else if(codigo.length() == 13) bulto = codigo.substring(6, 9);
+    public CodigoBarras getCodigoBarrasData(String codigo){ 
+        if((codigo.length() == 12 || codigo.length() == 13) 
+            && codigo.endsWith(PALABRA_FINAL)){
+                
+            String lote_extendido = codigo.substring(0, 6);
+            
+            String bulto = 
+                codigo.length() == 12 ? 
+                codigo.substring(6, 8) : 
+                codigo.substring(6, 9);
 
 		    return new CodigoBarras(codigo,lote_extendido,bulto);
         }
